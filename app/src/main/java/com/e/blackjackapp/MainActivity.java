a4f55e9ae15bcae88d51c9ab07ec3bfcf43de162
alyssaschilke <arschilke@loyola.edu>
2019-09-23 21:05:20 -0400

    main update

--- app/src/main/java/com/e/blackjackapp/MainActivity.java
+++ app/src/main/java/com/e/blackjackapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button2);
+        Button button = findViewById(R.id.b2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
