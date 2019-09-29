89b2dae5489dc43265c3d5a430799560cad93582
Aaron Pin <avpin@loyola.edu>
2019-09-29 15:12:53 -0400

    compare and hand counter player methods

--- app/src/main/java/com/e/blackjackapp/Player.java
+++ app/src/main/java/com/e/blackjackapp/Player.java
@@ -31,9 +31,56 @@ public class Player {
      *
      */
     private void tally() {
+        int aceCounter =0;
         for (int i = 0; i<hand.size(); i++){
-            points += hand.get(i).getValue();
+            if(hand.get(i).getName() == "Ace" ){
+                aceCounter++;
+            }
+            else {
+                points += hand.get(i).getValue();
+            }
         }
+
+        while(points>21){
+                if(aceCounter>0){
+                    points -= 10;
+                    aceCounter--;
+                }
+            }
+    }
+
+    /**
+     * -1: lose, 0: tie, 1:win, 2:player is closer, 3:dealer is closer
+     * @param p
+     */
+    public int comparePoints(Player p){
+        int playerPoints = p.points;
+        int dealerPoints = this.points;
+        if(playerPoints > 21){
+            return -1;
+        }
+        if(dealerPoints > 21){
+            return 1;
+        }
+        else {
+            //if player is closer to 21
+            if ((21 - playerPoints) > (21 - dealerPoints)) {
+
+                return 2;
+            }
+            //if dealer is closer to 21
+            if ((21 - playerPoints) < (21 - dealerPoints)) {
+
+                return 3;
+            }
+            //if both hand sums are equally close to 21
+            if ((21 - playerPoints) == (21 - dealerPoints)) {
+                return 0;
+            }
+        }
+        return 0;
+
+
     }
 
     /**
